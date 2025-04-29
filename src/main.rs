e3ffa64480743bed1671df57a6aa1ad864e136d6
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2025-04-29 13:49:18 -0400

    mwar

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ extern \"C\" __global__ void sin_kernel(float *out, const float *inp, int numel)
     let f = module.load_function("sin_kernel").unwrap();
     
     let mut a_host = vec![];
-    for i in 0..1000 {
+    for i in 0..100000 {
         a_host.push((i as f32)/100.0);
     }
 
